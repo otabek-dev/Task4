066ae15900e26dbbd52230ac87713ff5792df49d
Otabek Rustamov <otabek.pro@hotmail.com>
2023-05-27 15:11:10 +0500

    init new projcet

---
+++ WebAppTask4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppTask4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
